e05491e024fc53eeb3a62fb52614d39d62e26063
joy <iftekhar@example.com>
2022-12-11 20:29:15 +0600

    first code file

---
+++ hello.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+var addition int
+var one int
+var two int
+var x string
+
+// func add(int a, int b) {
+
+// 	addition = a + b
+// 	return addition
+
+// }
+
+func main() {
+
+	var firstname string
+	firstname = "xxx"
+	fmt.Println(firstname)
+
+	var lastname string
+	lastname = " ifte"
+
+	firstname = firstname + lastname
+	fmt.Println(firstname)
+
+	// fmt.Println("'this is my new line'")
+	one = 1
+	two = 2
+	x = "cash out done"
+	// fmt.Println(x, one, two)
+
+	// Declare variable
+
+	var x string = "hello"
+	var y string = "hello"
+	fmt.Println(x == y)
+	var yes = true
+	var no = false
+
+	fmt.Println(yes && no)
+
+	var familyName = "joy"
+	var surname = " ozil"
+	var numberTw0 = " 2"
+	var fullname = familyName + surname + numberTw0
+	var numberthree = 3
+	var x2 = 1
+	fmt.Println(fullname)
+	fmt.Println(numberthree + x2)
+
+}
